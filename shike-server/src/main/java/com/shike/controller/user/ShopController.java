ac7d71d36c11c6a3bdf293c5658e295b77a18888
coderXE丶 <2924401655@qq.com>
2026-09-25 10:50:41 +0800

    feat: 添加店铺营业状态获取与设置接口

--- shike-server/src/main/java/com/shike/controller/user/ShopController.java
+++ shike-server/src/main/java/com/shike/controller/user/ShopController.java
@@ -1,53 +1,36 @@
-package com.shike.controller.admin;
+package com.shike.controller.user;
 
 
 import com.shike.result.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.models.auth.In;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("adminShopController")
+@RestController("userShopController")
 @Slf4j
-@RequestMapping("/admin/shop")
+@RequestMapping("/user/status")
 @Api(tags = "店铺设置接口")
 public class ShopController {
-
+    // 类名相同 创建在IOC容器的bean 名称相同 是默认值 类名小写
     public static final String KEY = "SHOP_STATUS";
 
     @Autowired
-    RedisTemplate redisTemplate;
-
-
-    /**
-     * 设置店铺营业状态
-     *
-     * @param status
-     * @return
-     */
-    @PutMapping("/{status}")
-    @ApiOperation("设置店铺营业状态")
-    public Result setStatus(@PathVariable Integer status) {
-        log.info("设置店铺营业状态:{}", status == 1 ? "营业" : "打烊 ");
-
-        ValueOperations valueOperations = redisTemplate.opsForValue();
-
-        valueOperations.set(KEY, status);
-        return Result.success();
-    }
+    RedisTemplate<String,Object> redisTemplate;
 
     /**
      * 获取店铺状态
      */
     @GetMapping("/status")
     @ApiOperation("获取店铺营业状态")
-    public Result getStatus() {
+    public Result<Integer> getStatus() {
 
         Integer shopStatus =(Integer) redisTemplate.opsForValue().get(KEY);
         log.info("获取店铺的营业状态{}",shopStatus ==1 ? "营业" :"打烊");
-        return Result.success();
+        return Result.success(shopStatus);
     }
 }
